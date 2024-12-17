ea2d0b62f33460423761d97405d0a17ec983fcf7
OlhaFedorets <fedoretsolha@gmail.com>
2024-12-17 11:01:16 +0300

    change tests

--- src/01-hello-tests/01.test.ts
+++ src/01-hello-tests/01.test.ts
@@ -1,25 +1,13 @@
-// import {sum} from "./01";
-//
-// test('sum should be correct', () => {
-//     //data
-//     let a = 1;
-//     let b = 2;
-//     let c = 3;
-//
-//     //action
-//     const result = sum(a, b);
-//
-//     //expect result
-//     expect(result).toBe(3);
-// })
+import {mult, splitIntoWords, sum} from "./01";
 
-import {mult, sum} from "./01";
+//data
+const a = 1;
+const b = 2;
+const c = 3;
+
+
 
 test('sum should be correct', () => {
-    //data
-    let a = 1;
-    let b = 2;
-    let c = 3;
 
     //action
     const result1 = sum(a, b);
@@ -32,10 +20,6 @@ test('sum should be correct', () => {
 
 
 test('multiply should be correct', () => {
-    //data
-    let a = 1;
-    let b = 2;
-    let c = 3;
 
     //action
     const result1 = mult(a, b);
@@ -44,4 +28,27 @@ test('multiply should be correct', () => {
     //expect result
     expect(result1).toBe(2);
     expect(result2).toBe(6);
+})
+
+
+
+
+test('splitting into words should be correct', () => {
+    const sent1 = 'Hello my friend!'
+    const sent2 = 'JS - the best  programming language.'
+
+    const result1 = splitIntoWords(sent1)
+    const result2 = splitIntoWords(sent2)
+
+    expect(result1.length).toBe(3);
+    expect(result1[0]).toBe('hello');
+    expect(result1[1]).toBe('my');
+    expect(result1[2]).toBe('friend');
+
+    expect(result2.length).toBe(5);
+    expect(result2[0]).toBe('js');
+    expect(result2[1]).toBe('the');
+    expect(result2[2]).toBe('best');
+    expect(result2[3]).toBe('programming');
+    expect(result2[4]).toBe('language');
 })
